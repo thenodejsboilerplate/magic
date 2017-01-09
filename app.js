f419b9f909289a9f2c3bcda4530b5b5c1a74cf66
html5col <html5master@sina.com>
2017-01-09 13:11:17 +0800

    init

--- app.js
+++ app.js
@@ -11,19 +11,37 @@ require('./src/libs/mongoose-connect');
 
 //middlewares
 //Note: bodyParser() takes care of turning your request data into a JavaScript object on the server automatically.
-server.use(restify.bodyParser({ mapParams: true }));
-server.use(restify.queryParser({ mapParams: true }));
+server.use(restify.bodyParser());
+//Parses the HTTP query string (i.e., /foo?id=bar&name=mark). If you use this, the parsed content will always be available in req.query, additionally params are merged into req.params.
+server.use(restify.queryParser());
+//Parses out the Accept header, and ensures that the server can respond to what the client asked for. You almost always want to just pass in server.acceptable here, as that's an array of content types the server knows how to respond to (with the formatters you've registered). If the request is for a non-handled type, this plugin will return an error of 406.
+server.use(restify.acceptParser(server.acceptable));
+//server.use(restify.gzipResponse());
 
-server.get('/', function(req,res,next){
-  console.log('enteringinto frontpage');
-  res.send('frontpage');
-  next();
-});
-server.get('/about', function(req,res,next){
-  console.log('entering into about');
-  res.send('frontpage about page');
-  next();
-});
+// server.get('/', function(req,res,next){
+//   console.log('enteringinto frontpage');
+//   res.send('frontpage');
+//   next();
+// });
+// server.get('/about', function(req,res,next){
+//   console.log('entering into about');
+//   res.send('frontpage about page');
+//   next();
+// });
+
+server.get(/\/assets\/.*(js|css|png|jpg)$/, restify.serveStatic({
+  directory: './src/public'
+}));
+
+server.get(/\/?.*/, restify.serveStatic({
+  directory: './src/views',
+  default: 'index.html'
+}));
+
+// server.get(/\/assets\/?.*/, restify.serveStatic({
+//   directory: './assets',
+//  // default: 'forum-0304390e.css'
+// }));
 
 
 /**
@@ -31,7 +49,7 @@ server.get('/about', function(req,res,next){
  */
 server.on('uncaughtException', (req, res, route, err) => {
   console.error(err.stack);
-  res.json(err);
+  res.json(`error : ${err}`);
 });
 
 
